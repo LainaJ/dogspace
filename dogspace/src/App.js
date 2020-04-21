fcb5492f9839bdab26b8937212ce28ba1f8e8edc
LainaJ <lainakarosic@gmail.com>
2020-04-21 15:51:07 -0400

    title and site description layout

--- dogspace/src/App.js
+++ dogspace/src/App.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY
+
 
 class App extends Component {
   
+  state = {
+    videos: [],
+    displayVideo: null
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+          videos: videos,
+          selectedVideo: videos[0]
+      })
+  })
+
+}
+
+
   render() {
+    const videoSearch = _.debounce((term)=>{this.videoSearch(term)}, 300)
+    
     return (
       <div>
+        <div className="page-title">DogSpace</div>
+            <div className="page-description">A wellness space for dogs and their people</div>
+            {/* <SearchBar
+                onSearchTermChange={videoSearch}/>
+            <VideoDetail 
+                video={this.state.selectedVideo}/>
+            <VideoList 
+                onVideoSelect= {selectedVideo => this.setState({selectedVideo})}
+                videos ={this.state.videos}/> */}
+
+
       </div>
     )
   }
 
 
-  
+
 }
 
 export default App;
